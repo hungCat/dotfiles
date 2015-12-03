721e5f937d2a9b57093ed9b26225828881e39ac8
hungCat <hungcat4sheep@gmail.com>
2015-12-04 01:58:26 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,11 @@ for files in `find $dotfiles_dir -name '.*' -printf '%f\n'` ; do
 		ln -sf $dotfiles_dir/$files $home_dir/$files
 	fi
 done
+
+
+if [ -e $dotfiles_dir/.vim ]; then
+    # ファイルがある場合
+    mkdir -p $dotfiles_dir/.vim/bundle
+    #git clone https://github.com/Shougo/neobundle.vim $dotfiles_dir/.vim/bundle/neobundle.vim
+fi
+
