ca8dfee82483f62ac0ccdb5a7e8a9bc79f23bfd2
hungCat <hungcat4sheep@gmail.com>
2015-12-04 02:19:22 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,18 @@
 home_dir=$HOME
 dotfiles_dir=$HOME/dotfiles
 
+# NeoBundle用のディレクトリの作成
 mkdir -p $dotfiles_dir/.vim/bundle
 
+# dotfilesへのシンボリックリンクを貼る
 for files in `find $dotfiles_dir -name '.*' -printf '%f\n'` ; do
 	if [ $files != ".git" ]; then
 		ln -sf $dotfiles_dir/$files $home_dir/$files
 	fi
 done
 
-
-if [ -e $dotfiles_dir/.vim ]; then
-    #git clone https://github.com/Shougo/neobundle.vim $dotfiles_dir/.vim/bundle/neobundle.vim
+# NeoBundleが無かったらインストール
+if [ ! -e $dotfiles_dir/.vim/bundle/neobundle.vim/.git ]; then
+    git clone https://github.com/Shougo/neobundle.vim $dotfiles_dir/.vim/bundle/neobundle.vim
 fi
 
